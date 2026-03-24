ae0a0f02d443f77db243ba731a093054ab879590
AntoineduFresne <antoine@du-fresne.ch>
2026-03-24 13:50:35 +0100

    update

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,7 @@ package "MasterThesis" where
   -- add package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "v4.29.0-rc7"
-
-require cslib from git
-  "https://github.com/leanprover/cslib.git" @ "main"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.27.0"
 
 lean_lib «MasterThesis» where
   -- add library configuration options here
